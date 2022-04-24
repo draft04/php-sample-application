25b5b8eab938273c704dc4280ddd80a4021064a7
Juan Sebastian Leguizamon Archila <jsla9025@gmail.com>
2022-04-24 13:44:04 -0500

    add path to autload file

--- web/index.php
+++ web/index.php
@@ -1,22 +1,20 @@
 <?php
-
-require __DIR__ . '/vendor/autoload.php';
+echo "1";
+require __DIR__ . '/../vendor/autoload.php';
 ini_set('display_errors', 1);
-echo "step1";
-//$lastJoinedUsers = (require "dic/users.php")->getLastJoined();
-//echo "step2";
-//switch (require "dic/negotiated_format.php") {
-//    case "text/html":
-//        (new Views\Layout(
-//            "Twitter - Newcomers", new Views\Users\Listing($lastJoinedUsers), true
-//        ))();
-//        exit;
+$lastJoinedUsers = (require "dic/users.php")->getLastJoined();
+echo "index.php";
+switch (require "dic/negotiated_format.php") {
+    case "text/html":
+        (new Views\Layout(
+            "Twitter - Newcomers", new Views\Users\Listing($lastJoinedUsers), true
+        ))();
+        exit;
 
-//    case "application/json":
-//        header("Content-Type: application/json");
-//        echo json_encode($lastJoinedUsers);
-//        exit;
-//}
-//echo "step3";
-//http_response_code(406);
-//echo "end";
+    case "application/json":
+        header("Content-Type: application/json");
+        echo json_encode($lastJoinedUsers);
+        exit;
+}
+
+http_response_code(406);
