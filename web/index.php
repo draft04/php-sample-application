9bff94cd98bb7d1ab3e829cc20a7acc6e3d1b8f5
Juan Sebastian Leguizamon Archila <jsla9025@gmail.com>
2022-04-24 12:09:25 -0500

    add debug outputs

--- web/index.php
+++ web/index.php
@@ -2,8 +2,9 @@
 
 require __DIR__ . '/vendor/autoload.php';
 ini_set('display_errors', 1);
+echo "step1";
 $lastJoinedUsers = (require "dic/users.php")->getLastJoined();
-
+echo "step2";
 switch (require "dic/negotiated_format.php") {
     case "text/html":
         (new Views\Layout(
@@ -16,5 +17,6 @@ switch (require "dic/negotiated_format.php") {
         echo json_encode($lastJoinedUsers);
         exit;
 }
-
+echo "step3";
 http_response_code(406);
+echo "end";
