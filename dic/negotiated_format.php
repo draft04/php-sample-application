52bcd6890ec73a288d04567c36eacebf29720afa
Juan Sebastian Leguizamon Archila <jsla9025@gmail.com>
2022-04-24 14:22:18 -0500

    add PATH to autload.php

--- dic/negotiated_format.php
+++ dic/negotiated_format.php
@@ -1,5 +1,7 @@
 <?php
 
+require __DIR__ .  '/../vendor/autoload.php';
+
 /**
  * Returns the best negotiated format according to RFC 7231.
  */
--- dic/tweets.php
+++ dic/tweets.php
@@ -1,5 +1,7 @@
 <?php
 
+require __DIR__ .  '/../vendor/autoload.php';
+
 return new Service\TweetsService(
     require "config/db-connection.php"
 );
