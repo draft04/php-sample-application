708afa9f82e1705b2803b7fa875daae515b16ecd
Juan Sebastian Leguizamon Archila <jsla9025@gmail.com>
2022-04-24 12:12:32 -0500

    update index to test

--- web/index.php
+++ web/index.php
@@ -3,20 +3,20 @@
 require __DIR__ . '/vendor/autoload.php';
 ini_set('display_errors', 1);
 echo "step1";
-$lastJoinedUsers = (require "dic/users.php")->getLastJoined();
-echo "step2";
-switch (require "dic/negotiated_format.php") {
-    case "text/html":
-        (new Views\Layout(
-            "Twitter - Newcomers", new Views\Users\Listing($lastJoinedUsers), true
-        ))();
-        exit;
+//$lastJoinedUsers = (require "dic/users.php")->getLastJoined();
+//echo "step2";
+//switch (require "dic/negotiated_format.php") {
+//    case "text/html":
+//        (new Views\Layout(
+//            "Twitter - Newcomers", new Views\Users\Listing($lastJoinedUsers), true
+//        ))();
+//        exit;
 
-    case "application/json":
-        header("Content-Type: application/json");
-        echo json_encode($lastJoinedUsers);
-        exit;
-}
-echo "step3";
-http_response_code(406);
-echo "end";
+//    case "application/json":
+//        header("Content-Type: application/json");
+//        echo json_encode($lastJoinedUsers);
+//        exit;
+//}
+//echo "step3";
+//http_response_code(406);
+//echo "end";
